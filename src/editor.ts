cd6b016f61e5d583cca8d074cd2c5181daee07b9
dtinth on MBP M1 <dtinth@spacet.me>
2022-01-15 06:19:21 +0700

    try CodeMirror 6

---
+++ src/editor.ts
@@ -0,0 +1,58 @@
+import {
+  EditorView,
+  keymap,
+  highlightSpecialChars,
+  drawSelection,
+  highlightActiveLine,
+  dropCursor,
+} from '@codemirror/view'
+import { Extension, EditorState } from '@codemirror/state'
+import { history, historyKeymap } from '@codemirror/history'
+import { foldGutter, foldKeymap } from '@codemirror/fold'
+import { indentOnInput } from '@codemirror/language'
+import { lineNumbers, highlightActiveLineGutter } from '@codemirror/gutter'
+import { defaultKeymap } from '@codemirror/commands'
+import { bracketMatching } from '@codemirror/matchbrackets'
+import { closeBrackets, closeBracketsKeymap } from '@codemirror/closebrackets'
+import { searchKeymap, highlightSelectionMatches } from '@codemirror/search'
+import { autocompletion, completionKeymap } from '@codemirror/autocomplete'
+import { commentKeymap } from '@codemirror/comment'
+import { rectangularSelection } from '@codemirror/rectangular-selection'
+import { defaultHighlightStyle } from '@codemirror/highlight'
+import { lintKeymap } from '@codemirror/lint'
+import { StreamLanguage } from '@codemirror/stream-parser'
+import { ruby } from '@codemirror/legacy-modes/mode/ruby'
+
+export const setup: Extension = [
+  lineNumbers(),
+  highlightActiveLineGutter(),
+  highlightSpecialChars(),
+  history(),
+  foldGutter(),
+  drawSelection(),
+  dropCursor(),
+  EditorState.allowMultipleSelections.of(true),
+  indentOnInput(),
+  defaultHighlightStyle.fallback,
+  bracketMatching(),
+  closeBrackets(),
+  autocompletion(),
+  rectangularSelection(),
+  highlightActiveLine(),
+  highlightSelectionMatches(),
+  keymap.of([
+    ...closeBracketsKeymap,
+    ...defaultKeymap,
+    ...searchKeymap,
+    ...historyKeymap,
+    ...foldKeymap,
+    ...commentKeymap,
+    ...completionKeymap,
+    ...lintKeymap,
+  ]),
+  StreamLanguage.define(ruby),
+]
+
+Object.assign(window, {
+  cm: { EditorState, EditorView, setup, keymap },
+})
